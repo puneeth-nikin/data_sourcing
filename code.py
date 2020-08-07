c25a085f15b9a12937ca4ce3ae85394e9f6fd22a
Puneeth Nikin <puneethnikin@gmail.com>
2020-08-07 16:08:07 +0100

    minor modifications to format

--- code.py
+++ code.py
@@ -4,8 +4,6 @@ import zipfile
 import requests
 import time
 import os
-url = 'https://www.quantconnect.com/processDataDownload/22024/f06d6441f9c3785dd441b3db7b9972825f099fbcb61b7c5ba369a5fd5f09547b/20200322'
-r = requests.get(url, stream= True)
 
 
 def download_url(url, symbol, chunk_size=128):
@@ -31,13 +29,14 @@ def source_daily(symbol):
         data = pd.read_csv(path, header=None)
 
         data = data[range(0, 6, 1)]
-        data.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
-        data['datetime'] = pd.to_datetime(data['date'], format='%Y%m%d %H:%M')
-        data = data[['datetime', 'open', 'high', 'low', 'close']]
+        data.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+        data['Volume'] = 4
+        data['Date'] = pd.to_datetime(data['Date'], format='%Y%m%d %H:%M')
+        data = data[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
         data.to_csv('symbols/'+symbol+'/daily/'+symbol+'_daily.csv',index=False)
 
 def combine_organise(symbol):
-    final_data=pd.DataFrame(columns=['datetime','open','high','low','close','volume'])
+    final_data=pd.DataFrame(columns=['Date','Open','High','Low','Close','Volume'])
     zip_path = 'unzipped_files/symbols/' + symbol + '/minute/' + 'data'
     df=pd.DataFrame(columns=['path','file'])
     for file in os.listdir(zip_path):
@@ -50,17 +49,17 @@ def combine_organise(symbol):
                 data = pd.read_csv(path,header=None)
 
                 data = data[range(0,6,1)]
-                data.columns=['sec','open','high','low','close','volume']
-                data['date']=date
-                data['volume']=4
-                data['datetime']=pd.to_datetime(data['date'], format='%Y%m%d')+pd.to_timedelta(data['sec']/1000,unit='s')
-                data=data[['datetime','open','high','low','close','volume']]
-                if data.shape[0]>1:
+                data.columns=['sec','Open','High','Low','Close','Volume']
+                data['Date']=date
+                data['Volume']=4
+                data['Date']=pd.to_datetime(data['date'], format='%Y%m%d')+pd.to_timedelta(data['sec']/1000,unit='s')
+                data=data[['Date','Open','High','Low','Close','Volume']]
+                if data.shape[0]>2:
                     final_data=final_data.append(data)
 
         except:
             continue
-    final_data = final_data.sort_values(by='datetime')
+    final_data = final_data.sort_values(by='Date')
     final_data.to_csv('symbols/'+symbol+'/minute/'+symbol+'_minute.csv',index=False)
     print(final_data)
 
